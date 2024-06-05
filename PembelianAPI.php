bfe64ba11992073b3914adfc570c0eae64935636
SarahNelviandri <sarah.210170236@mhs.unimal.ac.id>
2024-06-05 13:01:18 +0700

    "Updated PembelianAPI.php: added kode_transaksi column, modified SELECT and INSERT queries, and updated PUT method to update status_pembayaran and total_harga."

--- PembelianAPI.php
+++ PembelianAPI.php
@@ -12,7 +12,7 @@ switch ($method) {
     case 'GET':
         if (isset($_GET['id'])) {
             $id = intval($_GET['id']);
-            $sql = "SELECT p.id_pembelian, pg.nama_pengguna, pr.nama_produk, pr.gambar_produk, pr.harga_produk, p.jumlah, p.total_harga, p.tanggal_pembelian, p.status_pembelian
+            $sql = "SELECT p.id_pembelian, pg.nama_pengguna, pr.nama_produk, pr.gambar_produk, pr.harga_produk, p.jumlah, p.total_harga, p.tanggal_pembelian, p.status_pembayaran, p.kode_transaksi
                     FROM tbl_pembelian p
                     JOIN tbl_pengguna pg ON p.id_pengguna = pg.id_pengguna
                     JOIN tbl_produk pr ON p.id_produk = pr.id_produk
@@ -26,7 +26,7 @@ switch ($method) {
             }
         } elseif (isset($_GET['id_pengguna'])) {
             $id_pengguna = intval($_GET['id_pengguna']);
-            $sql = "SELECT p.id_pembelian, pg.nama_pengguna, pr.nama_produk, pr.gambar_produk, pr.harga_produk, p.jumlah, p.total_harga, p.tanggal_pembelian, p.status_pembelian
+            $sql = "SELECT p.id_pembelian, pg.nama_pengguna, pr.nama_produk, pr.gambar_produk, pr.harga_produk, p.jumlah, p.total_harga, p.tanggal_pembelian, p.status_pembayaran, p.kode_transaksi
                     FROM tbl_pembelian p
                     JOIN tbl_pengguna pg ON p.id_pengguna = pg.id_pengguna
                     JOIN tbl_produk pr ON p.id_produk = pr.id_produk
@@ -38,7 +38,7 @@ switch ($method) {
             }
             echo json_encode($rows);
         } else {
-            $sql = "SELECT p.id_pembelian, pg.nama_pengguna, pr.nama_produk, pr.gambar_produk, pr.harga_produk, p.jumlah, p.total_harga, p.tanggal_pembelian, p.status_pembelian
+            $sql = "SELECT p.id_pembelian, pg.nama_pengguna, pr.nama_produk, pr.gambar_produk, pr.harga_produk, p.jumlah, p.total_harga, p.tanggal_pembelian, p.status_pembayaran, p.kode_transaksi
                     FROM tbl_pembelian p
                     JOIN tbl_pengguna pg ON p.id_pengguna = pg.id_pengguna
                     JOIN tbl_produk pr ON p.id_produk = pr.id_produk";
@@ -57,10 +57,21 @@ switch ($method) {
         $jumlah = $_POST['jumlah'];
         $total_harga = $_POST['total_harga'];
         $tanggal_pembelian = $_POST['tanggal_pembelian'];
-        $status_pembelian = $_POST['status_pembelian'];
+        $status_pembayaran = $_POST['status_pembayaran'];
 
-        $sql = "INSERT INTO tbl_pembelian (id_pengguna, id_produk, jumlah, total_harga, tanggal_pembelian, status_pembelian) 
-                VALUES ('$id_pengguna', '$id_produk', '$jumlah', '$total_harga', '$tanggal_pembelian', '$status_pembelian')";
+        // Generate automatic transaction code
+        $sql = "SELECT kode_transaksi FROM tbl_pembelian ORDER BY kode_transaksi DESC LIMIT 1";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $last_kode = $result->fetch_assoc()['kode_transaksi'];
+            $number = (int)substr($last_kode, 2) + 1;
+            $new_kode = 'PJ' . str_pad($number, 3, '0', STR_PAD_LEFT);
+        } else {
+            $new_kode = 'PJ001';
+        }
+
+        $sql = "INSERT INTO tbl_pembelian (id_pengguna, id_produk, jumlah, total_harga, tanggal_pembelian, status_pembayaran, kode_transaksi) 
+                VALUES ('$id_pengguna', '$id_produk', '$jumlah', '$total_harga', '$tanggal_pembelian', '$status_pembayaran', '$new_kode')";
 
         if ($conn->query($sql) === TRUE) {
             echo json_encode(array('status' => 'success', 'message' => 'Data pembelian berhasil ditambahkan'));
@@ -69,22 +80,25 @@ switch ($method) {
         }
         break;
 
-    case 'PUT':
-        $data = json_decode(file_get_contents('php://input'), true);
-        if (isset($data['id_pembelian']) && isset($data['status_pembelian'])) {
-            $id_pembelian = intval($data['id_pembelian']);
-            $status_pembelian = $conn->real_escape_string($data['status_pembelian']);
-
-            $sql = "UPDATE tbl_pembelian SET status_pembelian='$status_pembelian' WHERE id_pembelian=$id_pembelian";
-            if ($conn->query($sql) === TRUE) {
-                echo json_encode(array('status' => 'success', 'message' => 'Status pembelian berhasil diperbarui'));
+        case 'PUT':
+            $data = json_decode(file_get_contents('php://input'), true);
+            if (isset($data['id_pembelian']) && isset($data['status_pembayaran']) && isset($data['total_harga'])) {
+                $id_pembelian = intval($data['id_pembelian']);
+                $status_pembayaran = $conn->real_escape_string($data['status_pembayaran']);
+                $total_harga = $conn->real_escape_string($data['total_harga']);
+                $status_pembelian = 'Sudah Dibayar';
+        
+                $sql = "UPDATE tbl_pembelian SET status_pembayaran='$status_pembayaran', total_harga='$total_harga', status_pembelian='$status_pembelian' WHERE id_pembelian=$id_pembelian";
+                if ($conn->query($sql) === TRUE) {
+                    echo json_encode(array('status' => 'success', 'message' => 'Pembelian berhasil diperbarui'));
+                } else {
+                    echo json_encode(array('status' => 'error', 'message' => 'Gagal memperbarui pembelian'));
+                }
             } else {
-                echo json_encode(array('status' => 'error', 'message' => 'Gagal memperbarui status pembelian'));
+                echo json_encode(array('status' => 'error', 'message' => 'Data tidak lengkap'));
             }
-        } else {
-            echo json_encode(array('status' => 'error', 'message' => 'Data tidak lengkap'));
-        }
-        break;
+            break;
+        
 
     case 'DELETE':
         if (isset($_GET['id'])) {
